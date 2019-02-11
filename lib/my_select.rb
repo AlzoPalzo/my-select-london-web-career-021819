b6d7e09572278c96e9eb55adad62dee279deb65a
Ali Madni <ali555m@hotmail.com>
2019-02-11 08:21:48 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
  i = 0
  r_array = []
  while i < collection.length
-   collection << if yield collection[i] == true
-   end
+   if yield collection[i]
+     r_array << collection[i]
    i += 1
  end
  r_array
